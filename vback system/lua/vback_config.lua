a2a4e0798864564fb8100de1c7cb9061886740bb
DarkCide <eyon.schaefer@yahoo.com>
2016-03-19 23:15:44 -0500

    Added files via upload

---
+++ vback system/lua/vback_config.lua
@@ -0,0 +1,11 @@
+vback = {}
+vback["prefix"] = "[VBack] "
+vback["port"] = false
+vback["ip"] = { "192.168.0.5" }
+vback["kick"] = true
+vback["bantime"] = 1440
+vback["group"] = "owner"
+vback["echo"] = " - Attempted to use vback, unauthorized IP, kicking..."
+vback["reason"] = "Attempted to use vback, unauthorized IP!"
+
+MsgN( "vback_config.lua loaded" )
